eac5a4dfc94f1a4cd3fd38c447541f4af1c1e421
yuli setyo budi <yuli.setyo@gmail.com>
2019-02-25 11:16:22 +0700

    penambahan halaman kosong

---
+++ resources/views/page-blank.blade.php
@@ -0,0 +1,36 @@
+@extends('master')
+
+@section('sidemenu')
+					<?php echo $side_menu;?>
+@endsection
+
+@section('content')
+			<section class="content-header">
+				<h1>
+					<small><?php echo $nm_unit;?></small>
+				</h1>
+				<ol class="breadcrumb">
+					<li><a href="#"><i class="fa fa-dashboard"></i> Blank</a></li>
+					<li class="active">Blank</li>
+				</ol>
+			</section>
+
+			<section class="content">
+				<div class="box ">
+					<div class="box-header with-border">
+						<h3 class="box-title">
+							Header
+							<small></small>
+						</h3>
+					</div>
+					<div class="box-body"></div>
+				</div>
+			</section>
+
+<script>
+jQuery(document).ready(function(){
+	// jQuery kode
+	
+});
+</script>
+@endsection

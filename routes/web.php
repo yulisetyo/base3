ab455b975bdd9af32002f31fba6cd4127b1785fd
yuli setyo budi <yuli.setyo@gmail.com>
2019-02-25 11:08:28 +0700

    perbaikan routing

--- routes/web.php
+++ routes/web.php
@@ -32,12 +32,24 @@ Route::group(['middleware' => 'auth'], function() {
 	Route::get('/home', 'HomeController@index');
 
 	//routing utnuk surat masuk
-	Route::group(['prefix' => 'suratmasuk'], function(){
-			
+	Route::group(['prefix' => 'surat-masuk'], function(){
+		
 		Route::get('/', 'SuratmasukController@index');
 		Route::post('/', 'SuratmasukController@simpan');
+		Route::post('/terima', 'SuratmasukController@terima');
+		Route::post('/pinned', 'SuratmasukController@pinned');
+		Route::post('/unpinned', 'SuratmasukController@unpinned');
+		Route::get('/disposisi/{param}', 'SuratmasukController@disposisioning');
 		Route::get('/pilih/{param}', 'SuratmasukController@pilih');
-			
+		Route::get('/detail/{param}', 'SuratmasukController@detail');
+		Route::get('/isi-detail/{param}', 'SuratmasukController@isiDetail');
+		Route::get('/unreceived-tabel', 'SuratmasukController@suratMasukUnreceived');
+		Route::get('/unpushed-tabel', 'SuratmasukController@suratMasukUnpushed');
+		Route::get('/undisp-tabel', 'SuratmasukController@suratMasukUndisp');
+		Route::get('/tes-tabel', 'SuratmasukController@dataTable');
+		Route::get('/pdf/{param}', 'SuratmasukController@tayangPDF');
+		Route::get('/tes', 'SuratmasukController@tes');
+		
 	});
 
 	//routing untuk referensi
@@ -45,29 +57,41 @@ Route::group(['middleware' => 'auth'], function() {
 
 		//routing referensi user
 		Route::group(['prefix' => 'user'], function(){
+
 			Route::get('/', 'RefUserController@index');
+			Route::post('/reset', 'LoginController@reset');
+
 		});
 
 		//routing referensi sekretaris
 		Route::group(['prefix' => 'sekre'], function(){
+
 			Route::get('/', 'RefSekretarisController@index');
 			Route::get('/cek', 'RefSekretarisController@cekSekretaris');
+
 		});
 
 		//routing referensi pejabat plt
 		Route::group(['prefix' => 'plt'], function(){
+
 			Route::get('/', 'RefPltController@index');
+
 		});
 
 		//routing referensi pegawai
 		Route::group(['prefix' => 'pegawai'], function(){
+
 			Route::get('/dropdown', 'PegawaiController@dropdownPegawai');
+
 		});
 
 		//routing referensi unit
 		Route::group(['prefix' => 'unit'], function(){
+
+			Route::get('/', 'RefUnitController@index');
 			Route::get('/dropdown', 'RefUnitController@dropdownUnit');
 			Route::get('/vertikal', 'RefUnitController@unitVertikal');
+
 		});
 		
 	});
@@ -82,5 +106,6 @@ Route::group(['middleware' => 'auth'], function() {
 //route tes
 Route::group(['prefix' => 'tes'], function(){
 	Route::get('/', 'TesController@index');
+	Route::get('/foo', 'TesController@foo');
 });	
 
